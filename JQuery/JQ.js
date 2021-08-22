e4a49d63c1c0cb26247e7e0d3810b7cf2a91959f
Ahmed Radi <ahmedradi743@gmail.com>
2021-08-22 17:55:30 +0200

    made an only one button start and pause the carousel

--- JQuery/JQ.js
+++ JQuery/JQ.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
     $('#mycarousel').carousel({ interval:2000 });
-    $('#carousel-pause').click(function(){
-        $('#mycarousel').carousel('pause');
-    });
-    $('#carousel-play').click(function(){
-        $('#mycarousel').carousel('cycle');
+    $('#carouselButton').click(function(){
+        if($('#carouselButton').children('span').hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause')
+            $('#carouselButton').children('span').addClass('fa-play')
+        }else{
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play')
+            $('#carouselButton').children('span').addClass('fa-pause')
+        }
     });
 })
